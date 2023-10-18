43ec9532d5e15e7bca30ad4e105d03476763de8e
noobcoder <zhaoyi_114@outlook.com>
2023-10-18 16:03:05 +0800

    111

--- api-gateway/cmd/main.go
+++ api-gateway/cmd/main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"api-gateway/config"
 	"api-gateway/discovery"
+	"api-gateway/internal/service"
+	"api-gateway/routes"
+	"errors"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/resolver"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -15,5 +25,36 @@ func main() {
 	etcdAddress := []string{viper.GetString("etcd.address")}
 	etcdRegister := discovery.NewResolver(etcdAddress, logrus.New())
 	resolver.Register(etcdRegister)
+	go startListen()
+	{
+		osSignal := make(chan os.Signal, 1)
+		signal.Notify(osSignal, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
+		s := <-osSignal
+		logrus.Info("exit:", s)
+	}
+	fmt.Println("gate listen on:" + viper.GetString("server.port"))
+}
 
+func startListen() {
+	opts := []grpc.DialOption{}
+
+	userAddr := viper.GetString("userApi.Address")
+	userPort := viper.GetString("userApi.Port")
+	target := userAddr + ":" + userPort
+	userConn, _ := grpc.Dial(target, opts...)
+	userService := service.NewUserServiceClient(userConn)
+
+	ginRoter := routes.NewRouter(userService)
+	server := &http.Server{
+		Addr:           viper.GetString("server.port"),
+		Handler:        ginRoter,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(errors.New("启动失败" + err.Error()))
+	}
 }
